9ae5e5cb3eceb6a1c8909e7fbd881fe06a0cfec6
Nathan099-dev <tonininathan8@gmail.com>
2024-12-14 18:39:55 -0300

    criando código para grar uma nova investigação

--- public/js/funções.js
+++ public/js/funções.js
@@ -5,5 +5,9 @@ function criarInvestigacao(nome, dataDeInício, informações){
         informações,
     }
 
-    
+    //Salva as informações da investigação criada
+    let investigacao = JSON.parse(localStorage.getItem('investigacao'))
+    investigacao.push(novaInvestigacao)
+    localStorage.setItem('investigacao', JSON.stringify(investigacao));
+
 }
