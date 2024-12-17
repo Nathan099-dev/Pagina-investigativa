1c17e6a57371e466d073130dd70124dee19b4cff
Nathan099-dev <tonininathan8@gmail.com>
2024-12-17 09:46:22 -0300

    alterando o arquivo app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-import {express} from 'express';
-import {bodyParser} from 'body-parser';
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const investigacaoRouter = require('./models/investigacoes')
 const app = express();
-
 app.use(bodyParser.json())
 app.use(express.static('public'));
+
+
 
