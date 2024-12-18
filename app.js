409b45262eed628612cc6a47ea42ef13a6136f20
Nathan099-dev <tonininathan8@gmail.com>
2024-12-18 14:32:58 -0300

    configurando o aplicativo app.js

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const numVerifyRouter = require('./routes/numverify');
 
 const app = express();
 app.use(bodyParser.json())
+
+
+//rota de redirecionamento para a página de login
+app.get('/', (req, res) => {
+    res.redirect('/public/html/login.html');
+});
+
+//rota para fazer o gerenciamento das investigações
+app.get('/routes/investigacoes', investigacoesRouter);
+
+app.use('/routes/numverify', numVerifyRouter);
+
+//servir os arquivos estáticos
 app.use(express.static('public'));
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+});
 
 
