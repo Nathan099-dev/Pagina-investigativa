52fc2475eaecbd02ba750614507e2a6de5844ae7
Nathan099-dev <tonininathan8@gmail.com>
2024-12-19 14:20:18 -0300

    começando a escrever o arquivo config.js

---
+++ src/database/config.js
@@ -0,0 +1,20 @@
+const { connection } = require('mongoose');
+const sql = require('mysql2');
+
+const conexão = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password: 'natgambini234',
+    database: 'paginainvestigativa'
+});
+
+connection.connect(err => {
+    if (err) {
+        console.log('Erro na conexão com o banco de dados', err);
+        return;
+    }else{
+        console.log('Conectado ao banco  de dados mysql')
+    }
+
+    
+})
